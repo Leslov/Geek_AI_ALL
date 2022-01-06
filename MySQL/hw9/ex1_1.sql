155b8ea792ec446645439d4581393d8d4435faa6
Pavlov A <lehaha95@mail.ru>
2022-01-06 16:35:26 +0500

    Revert "MySQL hw9 частично"
    This reverts commit 29cfbb422f535e911fbc5c2926be561649ddd673.

--- MySQL/hw9/ex1_1.sql
+++
@@ -1,8 +0,0 @@
-START TRANSACTION;
-TRUNCATE sample.users;
-
-INSERT INTO sample.users SELECT * FROM shop.users WHERE shop.users.id = 1;
-
-SELECT * FROM sample.users;
-
-COMMIT;
--- MySQL/hw9/ex1_2.sql
+++
@@ -1,3 +0,0 @@
-CREATE OR REPLACE VIEW prod_in_catalog AS 
-SELECT prod.`name` as Product, cat.`name` as Catalog FROM products as prod
-INNER JOIN catalogs as cat ON cat.id = prod.catalog_id;
